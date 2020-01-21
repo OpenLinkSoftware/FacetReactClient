139d4bed60a79d9914de213f734ea662203f8c42
Carl Blakeley <cblakeley@openlinksw.com>
2020-01-21 09:17:54 +0000

    Added components for list and text-properties views

---
+++ src/FctRspLstRslt.js
@@ -0,0 +1,85 @@
+// A component to display the <fct:result type="list"> element of a Facet service response.
+
+import React from 'react';
+
+const componentContainerStyle = {
+  overflowX: 'auto',
+  overflowY: 'auto',
+  maxHeight: '200px',
+  maxWidth: '100%',
+}
+
+export default class FctRspLstRslt extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("FctRspLstRslt#constructor: props:", props);
+    if (props.qryResult["@type"] !== "list")
+      throw new Error(`Invalid Facet result type supplied. (${props.fctTextResult["@type"]})`);
+    this.state = { qryResult: props.qryResult };
+  }
+
+  render() {
+    let html = "Empty result set";
+    
+    if (this.state.qryResult && this.state.qryResult.row)
+    {
+      html = "";
+      
+      // Facet list result/view column mappings
+      const columnHeadings = ['', '']; // No headings in /fct version
+
+      let renderedHeadings = columnHeadings.map(heading => {
+        return `<th>${heading}</th>`;
+      });
+      renderedHeadings = `<thead><tr>${renderedHeadings.join('')}</tr></thead>`;
+
+      let rows = this.state.qryResult.row;
+      let renderedRows = rows.map((row) => {
+        let renderedCols = '';
+        let cols = row.column;
+        // cols[0]: item
+        // cols[1]: item label
+
+        let item = cols[0].keyValue;
+        let itemCurie = cols[0]["@shortform"];
+        let itemLabel = cols[1];
+        let itemColVal = '';
+
+        if (cols[0]["@datatype"] === "uri") {
+          if (itemLabel) {
+            itemColVal = `<a href="${item}">${itemLabel}</a>`;
+          }
+          else {
+            itemColVal = `<a href="${item}">${itemCurie}</a>`;
+          }
+        }
+        else
+          itemColVal = cols[0].keyValue.toString();
+        renderedCols += "<td>" + itemColVal + "</td>";
+
+        renderedCols += "<td>" + cols[0]["@datatype"]  + "</td>";
+
+        return "<tr>" + renderedCols + "</tr>";
+      })
+      renderedRows = `<tbody>${renderedRows.join('')}</tbody>`;
+
+      html = `
+      <div>
+      <span><em>values / FctRspLstRslt result:</em></span>` +
+        '<table class="table table-sm table-striped">' + 
+        renderedHeadings + 
+        renderedRows +
+        `</table>
+        </div>`;
+
+    }
+    
+    return ( 
+      <div 
+      style={componentContainerStyle} 
+      dangerouslySetInnerHTML={{__html: html}}>
+      </div> );
+  }
+
+}
---
+++ src/FctRspTxtPrprtsRslt.js
@@ -0,0 +1,94 @@
+// A component to display the <fct:result type="text-properties"> element of a Facet service response.
+
+// Examples:
+// View type 'text-properties' is triggered by filter link 'any attribute'.
+// e.g. ?s1 has [[any attribute]] with value "linked data".
+
+import React from 'react';
+
+const componentContainerStyle = {
+  overflowX: 'auto',
+  overflowY: 'auto',
+  maxHeight: '200px',
+  maxWidth: '100%',
+}
+
+export default class FctRspTxtPrprtsRslt extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("FctRspTxtPrprtsRslt#constructor: props:", props);
+    if (props.qryResult["@type"] !== "text-properties")
+      throw new Error(`Invalid Facet result type supplied. (${props.fctTextResult["@type"]})`);
+    this.state = { qryResult: props.qryResult };
+  }
+
+  render() {
+    let html = "Empty result set";
+    
+    if (this.state.qryResult && this.state.qryResult.row)
+    {
+      html = "";
+      
+      // Facet text-properties result/view column mappings
+      const columnHeadings = ['Attribute', 'Count'];
+
+      let renderedHeadings = columnHeadings.map(heading => {
+        return `<th>${heading}</th>`;
+      });
+      renderedHeadings = `<thead><tr>${renderedHeadings.join('')}</tr></thead>`;
+
+      let rows = this.state.qryResult.row;
+      let renderedRows = rows.map((row) => {
+        let renderedCols = '';
+        let cols = row.column;
+        // cols[0]: class URI (@keyValue) + class curie (@shortForm)
+        // cols[1]: text value
+        // cols[2]: occurrence count
+
+        let classURI = cols[0].keyValue;
+        let classCurie = cols[0]["@shortform"];
+        let typeColVal;
+
+        if (cols[0]["@datatype"] === "uri") {
+          // typeof cols[1] is 'string' or 'boolean'
+          if (typeof cols[1] === 'string') {
+            let classLabel = cols[1];
+            typeColVal = `<a href="${classURI}">${classLabel}</a>`;
+          }
+          else {
+            typeColVal = `<a href="${classURI}">${classCurie}</a>`;
+          }
+        }
+        else
+          typeColVal = cols[0].keyValue.toString();
+        renderedCols += "<td>" + typeColVal + "</td>";
+
+        let countColVal;
+        countColVal = Number(cols[2]);
+        countColVal = Number.isNaN(countColVal) ? 'NaN' : countColVal;
+        renderedCols += "<td>" + countColVal + "</td>";
+
+        return "<tr>" + renderedCols + "</tr>";
+      })
+      renderedRows = `<tbody>${renderedRows.join('')}</tbody>`;
+
+      html = `
+        <div>
+        <span><em>text-properties / FctRspTxtPrprts result:</em></span>` +
+        '<table class="table table-sm table-striped">' + 
+        renderedHeadings + 
+        renderedRows +
+        `</table>
+        </div>`;
+
+    }
+    
+    return ( 
+      <div 
+      style={componentContainerStyle} 
+      dangerouslySetInnerHTML={{__html: html}}>
+      </div> );
+  }
+
+}
